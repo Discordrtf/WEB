9400e42c69953dceca455fd6c5b2a2653cdac26a
Discordrtf <www.legenda_89@mail.ru>
2017-11-03 18:16:09 +0530

    Lab1

---
+++ Lab1/WebApplication5/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication5.Models;
+
+namespace WebApplication5.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Queries()
+        {
+            ViewData["Message"] = "";
+
+            return View();
+        }
+
+        public IActionResult Resources()
+        {
+            ViewData["Message"] = "";
+
+            return View();
+        }
+
+        public IActionResult Security()
+        {
+            ViewData["Message"] = "";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
